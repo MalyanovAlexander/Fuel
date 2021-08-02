b2cd876dbfde8e6c1590b45af60c0ddbcf5ce5fc
Александр Мальянов <malyanovalex@rambler.ru>
2021-08-02 12:11:43 +0300

    Рефакторинг контекста БД

--- Fuel/FuelDB.cs
+++ Fuel/FuelDB.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
@@ -11,71 +9,5 @@ namespace Fuel
     class FuelDB : DbContext
     {
         public DbSet<Trip> Trips { get; set; }
-    }
-
-    /// <summary>
-    /// Класс, описывающий километраж и расход топлива за день
-    /// </summary>
-    public class Trip
-    {
-        public int Id { get; set; }
-
-        [Required]
-        public DateTime Date { get; set; }
-
-        [Required]
-        /// <summary>
-        /// Спидометр утро
-        /// </summary>
-        public int MileageMorning { get; set; }
-
-        [Required]
-        /// <summary>
-        /// Остаток топлива утром
-        /// </summary>
-        public double FuelMorning { get; set; }
-
-        [Required]
-        /// <summary>
-        /// Заправка
-        /// </summary>
-        public int Refueling { get; set; }
-
-        [Required]
-        /// <summary>
-        /// Пробег суточный
-        /// </summary>
-        public int MileagePerDay { get; set; }
-
-        /// <summary>
-        /// Спидометр вечер
-        /// </summary>
-        public int MileageEvening { get; set; }
-
-        /// <summary>
-        /// Остаток топлива вечером
-        /// </summary>
-        public double FuelEvening { get; set; }
-
-        /// <summary>
-        /// Расход суточный
-        /// </summary>
-        public double DailyConsumption { get; set; }
-
-        [Required]
-        /// <summary>
-        /// Часы простоя
-        /// </summary>
-        public int IdleHours { get; set; }
-
-        /// <summary>
-        /// Расход простоя
-        /// </summary>
-        public double IdleConsumption { get; set; }
-
-        /// <summary>
-        /// Общий расход
-        /// </summary>
-        public double TotalConsumption { get; set; }
     }
 }

d8d74f6b64745fc305163f73a8f3370dfbb8066d
MalyanovAlexander <malyanov_91@mail.ru>
2021-08-21 13:06:56 +0300

    Первая миграция

---
+++ Fuel/Migrations/202108211005259_Initial.cs
@@ -0,0 +1,36 @@
+﻿namespace Fuel.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Trips",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Date = c.DateTime(nullable: false),
+                        MileageMorning = c.Int(nullable: false),
+                        FuelMorning = c.Double(nullable: false),
+                        Refueling = c.Int(nullable: false),
+                        MileagePerDay = c.Int(nullable: false),
+                        MileageEvening = c.Int(nullable: false),
+                        FuelEvening = c.Double(nullable: false),
+                        DailyConsumption = c.Double(nullable: false),
+                        IdleHours = c.Int(nullable: false),
+                        IdleConsumption = c.Double(nullable: false),
+                        TotalConsumption = c.Double(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Trips");
+        }
+    }
+}
